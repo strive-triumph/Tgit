ffbcdebb064a04c0a01922c7ae59bc8a8a729759
lhk <1042028773@qq.com>
2022-03-13 22:31:06 +0800

    b1

---
+++ src/main/java/B1.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: bai
+ * @DateTime: 2022/3/13 22:30
+ * @Description:
+ **/
+public class B1 {
+    public static void main(String[] args) {
+        System.out.println("b1");
+    }
+}
