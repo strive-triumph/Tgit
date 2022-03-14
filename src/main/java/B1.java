5382e0548508a0148613301e16277ebfb6440657
lhk <1042028773@qq.com>
2022-03-14 14:03:13 +0800

    我是master我修改了两个文件

--- src/main/java/B1.java
+++ src/main/java/B1.java
@@ -7,7 +7,8 @@ public class B1 {
     public static void main(String[] args) {
         System.out.println("b1");
         System.out.println("b2");
-        System.out.println("b3");
+        System.out.println("b6");
+        System.out.println("b4");
         System.out.println("b4");
     }
 }
