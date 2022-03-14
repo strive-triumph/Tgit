ed24149cfef69430178ea3a9e24010cc7f44642e
lhk <1042028773@qq.com>
2022-03-14 13:52:45 +0800

    机械师的第一次提交

---
+++ src/main/java/机械师/T.java
@@ -0,0 +1,13 @@
+package 机械师;
+
+/**
+ * @Author: bai
+ * @DateTime: 2022/3/14 13:52
+ * @Description:
+ **/
+public class T {
+    public static void main(String[] args) {
+        System.out.println(1);
+
+    }
+}
