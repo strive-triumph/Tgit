f1d8ddf6290b56a5f3be72fa7369f84ccaeb9b31
lhk <1042028773@qq.com>
2022-03-14 14:58:02 +0800

    开发了新功能2

--- src/main/java/开发了新功能/A.java
+++ src/main/java/开发了新功能/A.java
@@ -6,4 +6,11 @@ package 开发了新功能;
  * @Description:
  **/
 public class A {
+    public static void main(String[] args) {
+        System.out.println("新加");
+        System.out.println("新加2");
+
+
+
+    }
 }
