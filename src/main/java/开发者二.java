2b7ce181aac2278c838ac32b5a36d029c34f3595
lhk <1042028773@qq.com>
2022-03-14 14:37:36 +0800

    机械师修改了master

--- src/main/java/开发者二.java
+++ src/main/java/开发者二.java
@@ -1,7 +1,8 @@
 public class 开发者二 {
     public static void main(String[] args) {
         System.out.println("开发2");
-        System.out.println("开发者2第二次提交");
-        System.out.println("开发者2第三次提交");
+        System.out.println("v");
+        System.out.println("这是他们错的我修改了");
+        System.out.println("我是机械师我也加了东西");
     }
 }
