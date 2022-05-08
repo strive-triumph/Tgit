70b89063ab261371787aaae0292298f11748ce9e
lhk <1042028773@qq.com>
2022-05-08 21:03:05 +0800

    hotfix开发

--- src/main/java/有开发了新功能/A.java
+++ src/main/java/有开发了新功能/A.java
@@ -9,5 +9,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("release");
+        System.out.println("hotfix");
     }
 }
